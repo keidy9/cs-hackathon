b33890d7f5fdb0a532979a8840fe8f0654978013
StephCummins <steph.a.cummins@gmail.com>
2023-10-12 08:58:47 -0700

    Updated Chrome Extension

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ box.style.pointerEvents = 'none';
 
 const hiddenImage = document.createElement('img');
 hiddenImage.setAttribute('id', 'shrek')
-// hiddenImage.src = 'https://pngimg.com/d/shrek_PNG16.png';
 hiddenImage.src = 'https://imgb.ifunny.co/images/816cd88683311a5fe384c98b64fbea02d21e45d83300ac9578869dc269a9abc6_1.jpg';
 hiddenImage.style.width = "23px";
 hiddenImage.style.height = "30px";
@@ -38,21 +37,48 @@ if (hiddenImage) {
         newVidBox.prepend(videoSrc);
         
         videoSrc.style.pointerEvents = "auto"; 
+        const largeHankBob = document.createElement('img');
+        largeHankBob.src = 'https://imgb.ifunny.co/images/816cd88683311a5fe384c98b64fbea02d21e45d83300ac9578869dc269a9abc6_1.jpg';
+        largeHankBob.style.width = "200px";
+        largeHankBob.style.height = "250px";
+        newVidBox.appendChild(largeHankBob);
         
         box.prepend(newVidBox);
+        hiddenImage.style.visibility = 'hidden';
 
-        // const audio = new Audio('https://quicksounds.com/sound/13454/theatre-applause');
+        videoSrc.style.position = "absolute";
+        largeHankBob.style.position = "absolute";
+        videoSrc.marginTop = '100px';
+
+        videoSrc.style.zIndex = 1;
+        largeHankBob.style.zIndex = 2;
+
+        videoSrc.style.transform = 'translate(-50%, 100%)';
+        largeHankBob.style.transform = 'translate(-50%, 140%)';
+
+        const audio = new Audio();
+        audio.src = 'https://www.myinstants.com/en/instant/hank-hill-bwah-59976/';
+        audio.autoplay = true;
+        audio.play();
+        // audio.addEventListener("load", function() {
+        //     audio.play;
+        // })
+        
+
+        // const audio = new Audio('https://www.myinstants.com/en/instant/hank-hill-bwah-59976/');
         // audio.play();
 
-        const winAudio = document.createElement('audio');
-        const audioSrc = document.createElement('source');
-        audioSrc.src = 'https://quicksounds.com/sound/13454/theatre-applause';
-        winAudio.prepend(audioSrc);
-        winAudio.setAttribute('autoplay', true);
-        box.prepend(winAudio);
+        // const winAudio = document.createElement('audio');
+        // const audioSrc = document.createElement('source');
+        // audioSrc.src = 'https://www.myinstants.com/en/instant/hank-hill-bwah-59976/?utm_source=copy&utm_medium=share';
+        // winAudio.prepend(audioSrc);
+        // winAudio.autoplay = true;
+        // //winAudio.setAttribute('autoplay', true);
+        // box.prepend(winAudio);
 
         setTimeout(() => {
             newVidBox.parentElement.removeChild(newVidBox);
+            hiddenImage.style.visibility = 'visible';
             hiddenImage.style.top = `${Math.random()* 100}%`;
             hiddenImage.style.left = `${Math.random()* 100}%`;
         }, 5000)

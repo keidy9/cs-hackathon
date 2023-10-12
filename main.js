82e895ea2cd1fce0e070e9dd71f4872ffbbff427
keidy9 <keidyw999@gmail.com>
2023-10-12 10:01:14 -0700

    uses local files now

--- main.js
+++ main.js
@@ -1,91 +1,91 @@
-const pageBody = document.querySelector('body');
+const pageBody = document.querySelector("body");
 console.log(pageBody);
-const box = document.createElement('div');
-box.style.height = '100%';
-box.style.width = '100%';
-box.style.position = 'fixed';
-box.style.top = '0';
-box.style.zIndex= '999';
-box.style.pointerEvents = 'none';
+const box = document.createElement("div");
+box.style.height = "100%";
+box.style.width = "100%";
+box.style.position = "fixed";
+box.style.top = "0";
+box.style.zIndex = "999";
+box.style.pointerEvents = "none";
 
-const hiddenImage = document.createElement('img');
-hiddenImage.setAttribute('id', 'shrek')
-hiddenImage.src = 'https://imgb.ifunny.co/images/816cd88683311a5fe384c98b64fbea02d21e45d83300ac9578869dc269a9abc6_1.jpg';
+const hiddenImage = document.createElement("img");
+hiddenImage.setAttribute("id", "shrek");
+hiddenImage.src =
+  "https://imgb.ifunny.co/images/816cd88683311a5fe384c98b64fbea02d21e45d83300ac9578869dc269a9abc6_1.jpg";
 hiddenImage.style.width = "23px";
 hiddenImage.style.height = "30px";
 hiddenImage.style.position = "absolute";
-hiddenImage.style.pointerEvents = "auto"; 
+hiddenImage.style.pointerEvents = "auto";
 
-hiddenImage.style.top = `${Math.random()* 100}%`;
-hiddenImage.style.left = `${Math.random()* 100}%`;
+hiddenImage.style.top = `${Math.random() * 100}%`;
+hiddenImage.style.left = `${Math.random() * 100}%`;
 
 box.prepend(hiddenImage);
 pageBody.prepend(box);
 
-pageBody.addEventListener('click', () => console.log("You clicked!"));
+pageBody.addEventListener("click", () => console.log("You clicked!"));
 
 if (hiddenImage) {
-    console.log("Hidden image added");
-    console.log(hiddenImage);
-    hiddenImage.addEventListener('click', () => {
-        const videoSrc = document.createElement('img');
-        videoSrc.src = 'https://media4.giphy.com/media/26tOZ42Mg6pbTUPHW/giphy.gif';
-    
-        const newVidBox = document.createElement('p');
-        newVidBox.style.textAlign = "center";
+  console.log("Hidden image added");
+  console.log(hiddenImage);
+  hiddenImage.addEventListener("click", () => {
+    const videoSrc = document.createElement("img");
+    videoSrc.src = "https://media4.giphy.com/media/26tOZ42Mg6pbTUPHW/giphy.gif";
 
-        newVidBox.prepend(videoSrc);
-        
-        videoSrc.style.pointerEvents = "auto"; 
-        const largeHankBob = document.createElement('img');
-        largeHankBob.src = 'https://imgb.ifunny.co/images/816cd88683311a5fe384c98b64fbea02d21e45d83300ac9578869dc269a9abc6_1.jpg';
-        largeHankBob.style.width = "200px";
-        largeHankBob.style.height = "250px";
-        newVidBox.appendChild(largeHankBob);
-        
-        box.prepend(newVidBox);
-        hiddenImage.style.visibility = 'hidden';
+    const newVidBox = document.createElement("p");
+    newVidBox.style.textAlign = "center";
 
-        videoSrc.style.position = "absolute";
-        largeHankBob.style.position = "absolute";
-        videoSrc.marginTop = '100px';
+    newVidBox.prepend(videoSrc);
 
-        videoSrc.style.zIndex = 1;
-        largeHankBob.style.zIndex = 2;
+    videoSrc.style.pointerEvents = "auto";
+    const largeHankBob = document.createElement("img");
+    // largeHankBob.src =
+      //   "https://imgb.ifunny.co/images/816cd88683311a5fe384c98b64fbea02d21e45d83300ac9578869dc269a9abc6_1.jpg";
+      largeHankBob.src = chrome.runtime.getURL("assets/hank.jpg");
+    largeHankBob.style.width = "200px";
+    largeHankBob.style.height = "250px";
+    newVidBox.appendChild(largeHankBob);
 
-        videoSrc.style.transform = 'translate(-50%, 100%)';
-        largeHankBob.style.transform = 'translate(-50%, 140%)';
+    box.prepend(newVidBox);
+    hiddenImage.style.visibility = "hidden";
 
-        const audio = new Audio();
-        audio.src = 'https://www.myinstants.com/en/instant/hank-hill-bwah-59976/';
-        audio.autoplay = true;
-        audio.play();
-        // audio.addEventListener("load", function() {
-        //     audio.play;
-        // })
-        
+    videoSrc.style.position = "absolute";
+    largeHankBob.style.position = "absolute";
+    videoSrc.marginTop = "100px";
 
-        // const audio = new Audio('https://www.myinstants.com/en/instant/hank-hill-bwah-59976/');
-        // audio.play();
+    videoSrc.style.zIndex = 1;
+    largeHankBob.style.zIndex = 2;
 
-        // const winAudio = document.createElement('audio');
-        // const audioSrc = document.createElement('source');
-        // audioSrc.src = 'https://www.myinstants.com/en/instant/hank-hill-bwah-59976/?utm_source=copy&utm_medium=share';
-        // winAudio.prepend(audioSrc);
-        // winAudio.autoplay = true;
-        // //winAudio.setAttribute('autoplay', true);
-        // box.prepend(winAudio);
+    videoSrc.style.transform = "translate(-50%, 100%)";
+    largeHankBob.style.transform = "translate(-50%, 140%)";
 
-        setTimeout(() => {
-            newVidBox.parentElement.removeChild(newVidBox);
-            hiddenImage.style.visibility = 'visible';
-            hiddenImage.style.top = `${Math.random()* 100}%`;
-            hiddenImage.style.left = `${Math.random()* 100}%`;
-        }, 5000)
-        
-    });
+    const audio = new Audio();
+    audio.src = "https://www.myinstants.com/en/instant/hank-hill-bwah-59976/";
+    audio.autoplay = true;
+    audio.play();
+    // audio.addEventListener("load", function() {
+    //     audio.play;
+    // })
+
+    // const audio = new Audio('https://www.myinstants.com/en/instant/hank-hill-bwah-59976/');
+    // audio.play();
+
+    // const winAudio = document.createElement('audio');
+    // const audioSrc = document.createElement('source');
+    // audioSrc.src = 'https://www.myinstants.com/en/instant/hank-hill-bwah-59976/?utm_source=copy&utm_medium=share';
+    // winAudio.prepend(audioSrc);
+    // winAudio.autoplay = true;
+    // //winAudio.setAttribute('autoplay', true);
+    // box.prepend(winAudio);
+
+    setTimeout(() => {
+      newVidBox.parentElement.removeChild(newVidBox);
+      hiddenImage.style.visibility = "visible";
+      hiddenImage.style.top = `${Math.random() * 100}%`;
+      hiddenImage.style.left = `${Math.random() * 100}%`;
+    }, 5000);
+  });
 }
-
 
 // SEMI-WORKING CODE:
 // const pageBody = document.querySelector('body');
@@ -102,11 +102,10 @@ if (hiddenImage) {
 // hiddenImage.src = 'https://i.kym-cdn.com/entries/icons/original/000/031/320/poster_840x830_f8f8f8-pad_750x1000_f8f8f8.jpg';
 // hiddenImage.style.width = "25px";
 // hiddenImage.style.position = "absolute";
-// hiddenImage.style.pointerEvents = "auto"; 
+// hiddenImage.style.pointerEvents = "auto";
 
 // hiddenImage.style.top = `${Math.random()* 100}%`;
 // hiddenImage.style.left = `${Math.random()* 100}%`;
-
 
 // box.prepend(hiddenImage);
 // // box.style.backgroundColor = "blue";
@@ -125,32 +124,14 @@ if (hiddenImage) {
 //     });
 // }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // SHREK CODE -> DO NOT DELETE!
 
 // const img = document.querySelectorAll('img');
 // for (let image in img) {
-//     console.log(img[image]); 
+//     console.log(img[image]);
 //     img[image].src = '';
 //     img[image].srcset = 'https://i.kym-cdn.com/entries/icons/original/000/031/320/poster_840x830_f8f8f8-pad_750x1000_f8f8f8.jpg';
-//     console.log(img[image]); 
-
+//     console.log(img[image]);
 
 // }
 // console.log(img);
-

0df4e0b26b4b1e9f960e65b029dc475b802ee636
StephCummins <steph.a.cummins@gmail.com>
2023-10-12 00:14:34 -0700

    Final

--- main.js
+++ main.js
@@ -1,30 +1,26 @@
-document.addEventListener('loaded', () => {
-
-
-
-
-    const pageBody = document.querySelector('body');
-    const box = document.createElement('div');
-    box.style.height = '95%';
-    box.style.width = '95%';
-    box.style.position = 'fixed';
-    box.style.top = '0';
-    box.style.zIndex= '999';
+const pageBody = document.querySelector('body');
+console.log(pageBody);
+const box = document.createElement('div');
+box.style.height = '100%';
+box.style.width = '100%';
+box.style.position = 'fixed';
+box.style.top = '0';
+box.style.zIndex= '999';
 box.style.pointerEvents = 'none';
 
 const hiddenImage = document.createElement('img');
 hiddenImage.setAttribute('id', 'shrek')
-hiddenImage.src = 'https://i.kym-cdn.com/entries/icons/original/000/031/320/poster_840x830_f8f8f8-pad_750x1000_f8f8f8.jpg';
-hiddenImage.style.width = "25px";
+// hiddenImage.src = 'https://pngimg.com/d/shrek_PNG16.png';
+hiddenImage.src = 'https://imgb.ifunny.co/images/816cd88683311a5fe384c98b64fbea02d21e45d83300ac9578869dc269a9abc6_1.jpg';
+hiddenImage.style.width = "23px";
+hiddenImage.style.height = "30px";
 hiddenImage.style.position = "absolute";
 hiddenImage.style.pointerEvents = "auto"; 
 
 hiddenImage.style.top = `${Math.random()* 100}%`;
 hiddenImage.style.left = `${Math.random()* 100}%`;
 
-
 box.prepend(hiddenImage);
-// box.style.backgroundColor = "blue";
 pageBody.prepend(box);
 
 pageBody.addEventListener('click', () => console.log("You clicked!"));
@@ -33,22 +29,75 @@ if (hiddenImage) {
     console.log("Hidden image added");
     console.log(hiddenImage);
     hiddenImage.addEventListener('click', () => {
-        console.log("You found Shrek!");
-        const video = document.createElement('video');
-        video.src = 'https://youtu.be/QGJuMBdaqIw?si=iNi64lwnTXVIjT4y&t=60&autoplay=1';
-        box.prepend(video);
+        const videoSrc = document.createElement('img');
+        videoSrc.src = 'https://media4.giphy.com/media/26tOZ42Mg6pbTUPHW/giphy.gif';
+    
+        const newVidBox = document.createElement('p');
+        newVidBox.style.textAlign = "center";
+
+        newVidBox.prepend(videoSrc);
+        
+        videoSrc.style.pointerEvents = "auto"; 
+        
+        box.prepend(newVidBox);
+
+        // const audio = new Audio('https://quicksounds.com/sound/13454/theatre-applause');
+        // audio.play();
+
+        const winAudio = document.createElement('audio');
+        const audioSrc = document.createElement('source');
+        audioSrc.src = 'https://quicksounds.com/sound/13454/theatre-applause';
+        winAudio.prepend(audioSrc);
+        winAudio.setAttribute('autoplay', true);
+        box.prepend(winAudio);
+
+        setTimeout(() => {
+            newVidBox.parentElement.removeChild(newVidBox);
+            hiddenImage.style.top = `${Math.random()* 100}%`;
+            hiddenImage.style.left = `${Math.random()* 100}%`;
+        }, 5000)
+        
     });
 }
 
 
-})
+// SEMI-WORKING CODE:
+// const pageBody = document.querySelector('body');
+// const box = document.createElement('div');
+// box.style.height = '95%';
+// box.style.width = '95%';
+// box.style.position = 'fixed';
+// box.style.top = '0';
+// box.style.zIndex= '999';
+// box.style.pointerEvents = 'none';
+
+// const hiddenImage = document.createElement('img');
+// hiddenImage.setAttribute('id', 'shrek')
+// hiddenImage.src = 'https://i.kym-cdn.com/entries/icons/original/000/031/320/poster_840x830_f8f8f8-pad_750x1000_f8f8f8.jpg';
+// hiddenImage.style.width = "25px";
+// hiddenImage.style.position = "absolute";
+// hiddenImage.style.pointerEvents = "auto"; 
+
+// hiddenImage.style.top = `${Math.random()* 100}%`;
+// hiddenImage.style.left = `${Math.random()* 100}%`;
 
 
+// box.prepend(hiddenImage);
+// // box.style.backgroundColor = "blue";
+// pageBody.prepend(box);
 
+// pageBody.addEventListener('click', () => console.log("You clicked!"));
 
-
-
-
+// if (hiddenImage) {
+//     console.log("Hidden image added");
+//     console.log(hiddenImage);
+//     hiddenImage.addEventListener('click', () => {
+//         console.log("You found Shrek!");
+//         const video = document.createElement('video');
+//         video.src = 'https://youtu.be/QGJuMBdaqIw?si=iNi64lwnTXVIjT4y&t=60&autoplay=1';
+//         box.prepend(video);
+//     });
+// }
 
 
 

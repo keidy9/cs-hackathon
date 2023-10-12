1c5a53396549230899465737f14f698649bf3821
StephCummins <steph.a.cummins@gmail.com>
2023-10-12 09:56:33 -0700

    Finalized HankBob Chrome Extension

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ box.style.zIndex= '999';
 box.style.pointerEvents = 'none';
 
 const hiddenImage = document.createElement('img');
-hiddenImage.setAttribute('id', 'shrek')
 hiddenImage.src = 'https://imgb.ifunny.co/images/816cd88683311a5fe384c98b64fbea02d21e45d83300ac9578869dc269a9abc6_1.jpg';
 hiddenImage.style.width = "23px";
 hiddenImage.style.height = "30px";
@@ -57,100 +56,16 @@ if (hiddenImage) {
         largeHankBob.style.transform = 'translate(-50%, 140%)';
 
         const audio = new Audio();
-        audio.src = 'https://www.myinstants.com/en/instant/hank-hill-bwah-59976/';
+        audio.src = chrome.runtime.getURL("assets/hank-audio.mp3");
         audio.autoplay = true;
         audio.play();
-        // audio.addEventListener("load", function() {
-        //     audio.play;
-        // })
         
-
-        // const audio = new Audio('https://www.myinstants.com/en/instant/hank-hill-bwah-59976/');
-        // audio.play();
-
-        // const winAudio = document.createElement('audio');
-        // const audioSrc = document.createElement('source');
-        // audioSrc.src = 'https://www.myinstants.com/en/instant/hank-hill-bwah-59976/?utm_source=copy&utm_medium=share';
-        // winAudio.prepend(audioSrc);
-        // winAudio.autoplay = true;
-        // //winAudio.setAttribute('autoplay', true);
-        // box.prepend(winAudio);
-
         setTimeout(() => {
             newVidBox.parentElement.removeChild(newVidBox);
             hiddenImage.style.visibility = 'visible';
             hiddenImage.style.top = `${Math.random()* 100}%`;
             hiddenImage.style.left = `${Math.random()* 100}%`;
-        }, 5000)
+        }, 1000)
         
     });
 }
-
-
-// SEMI-WORKING CODE:
-// const pageBody = document.querySelector('body');
-// const box = document.createElement('div');
-// box.style.height = '95%';
-// box.style.width = '95%';
-// box.style.position = 'fixed';
-// box.style.top = '0';
-// box.style.zIndex= '999';
-// box.style.pointerEvents = 'none';
-
-// const hiddenImage = document.createElement('img');
-// hiddenImage.setAttribute('id', 'shrek')
-// hiddenImage.src = 'https://i.kym-cdn.com/entries/icons/original/000/031/320/poster_840x830_f8f8f8-pad_750x1000_f8f8f8.jpg';
-// hiddenImage.style.width = "25px";
-// hiddenImage.style.position = "absolute";
-// hiddenImage.style.pointerEvents = "auto"; 
-
-// hiddenImage.style.top = `${Math.random()* 100}%`;
-// hiddenImage.style.left = `${Math.random()* 100}%`;
-
-
-// box.prepend(hiddenImage);
-// // box.style.backgroundColor = "blue";
-// pageBody.prepend(box);
-
-// pageBody.addEventListener('click', () => console.log("You clicked!"));
-
-// if (hiddenImage) {
-//     console.log("Hidden image added");
-//     console.log(hiddenImage);
-//     hiddenImage.addEventListener('click', () => {
-//         console.log("You found Shrek!");
-//         const video = document.createElement('video');
-//         video.src = 'https://youtu.be/QGJuMBdaqIw?si=iNi64lwnTXVIjT4y&t=60&autoplay=1';
-//         box.prepend(video);
-//     });
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// SHREK CODE -> DO NOT DELETE!
-
-// const img = document.querySelectorAll('img');
-// for (let image in img) {
-//     console.log(img[image]); 
-//     img[image].src = '';
-//     img[image].srcset = 'https://i.kym-cdn.com/entries/icons/original/000/031/320/poster_840x830_f8f8f8-pad_750x1000_f8f8f8.jpg';
-//     console.log(img[image]); 
-
-
-// }
-// console.log(img);
-

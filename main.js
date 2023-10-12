646db5098bf2111c7bf920075e02c2db440d7458
StephCummins <steph.a.cummins@gmail.com>
2023-10-11 20:14:12 -0700

    Shrek project

---
+++ main.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SHREK CODE -> DO NOT DELETE!
+
+// const img = document.querySelectorAll('img');
+// for (let image in img) {
+//     console.log(img[image]); 
+//     img[image].src = 'https://i.kym-cdn.com/entries/icons/original/000/031/320/poster_840x830_f8f8f8-pad_750x1000_f8f8f8.jpg';
+//     img[image].srcset = 'https://i.kym-cdn.com/entries/icons/original/000/031/320/poster_840x830_f8f8f8-pad_750x1000_f8f8f8.jpg';
+//     console.log(img[image]); 
+
+
+// }
+// console.log(img);
+
